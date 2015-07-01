5dc70b795369eb6c9dd64dcdf445263d2f270697
Deign35 <squarepiestudios@gmail.com>
2015-07-01 15:10:50 -0700

    Example code to demonstrate the broken SerializedProperty functionality

---
+++ BrokenInspectorObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class BrokenInspectorObject {
+    [SerializeField]
+    public List<char> CharList1 = new List<char>();
+    [SerializeField]
+    public List<char> CharList2 = new List<char>();
+}
---
+++ BrokenInspectorScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+
+public class BrokenInspectorScript : MonoBehaviour {
+    public BrokenInspectorObject ObjectToBreak;
+
+    private void Start() {
+        for (int i = 0; i < ObjectToBreak.CharList1.Count; i++) {
+            Debug.LogFormat("ObjectToBreak's CharList1[{0}] = {1}", i, ObjectToBreak.CharList1[i]);
+        }
+        for (int i = 0; i < ObjectToBreak.CharList2.Count; i++) {
+            Debug.LogFormat("ObjectToBreak's CharList2[{0}] = {1}", i, ObjectToBreak.CharList2[i]);
+        }
+    }
+}
